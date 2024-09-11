be8f99d79064549ef0fa2c17848de44cc50eed59
AKshay Mausam <akshayjha560@gmail.com>
2024-09-11 23:47:28 +0530

    added all input validations like @NotNull and all

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto.Employe
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities.EmployeeEntity;
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.repositories.EmployeeRepository;
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.services.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,11 +38,11 @@ public class EmployeeController {
     }
 
     @PostMapping()
-    public ResponseEntity<EmployeeDTO> createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+    public ResponseEntity<EmployeeDTO> createNewEmployee(@RequestBody @Valid EmployeeDTO inputEmployee){
        EmployeeDTO savedEmployee=employeeService.createNewEmployee(inputEmployee);
        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
-
+//it updates all or multipe fields
     @PutMapping(path="/{employeeId}")
     public ResponseEntity<EmployeeDTO> updateEmployeeById(@RequestBody EmployeeDTO employeeDTO, @PathVariable Long employeeId){
         return ResponseEntity.ok(employeeService.updateEmployeeById(employeeId,employeeDTO));
@@ -53,7 +54,7 @@ public class EmployeeController {
       if(gotDeleted) return ResponseEntity.ok(true);
       return ResponseEntity.notFound().build();
     }
-
+//it updates only certain fields
     @PatchMapping(path="/{employeeId}")
     public ResponseEntity<EmployeeDTO> updatePartialEmployeeById(@RequestBody Map<String,Object> updates, @PathVariable Long employeeId){
        EmployeeDTO employeeDTO= employeeService.updatePartialEmployeeById(employeeId,updates);
--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/dto/EmployeeDTO.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -9,13 +10,36 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class EmployeeDTO {
     private Long id;
-
+   @NotBlank(message="Required field in employee : Name")
+   @NotEmpty(message = "name cannot be empty")
+   @Size(min=3, max=10, message="number of character should be in range")
     private String name;
+   @Email(message = "email should be a valid email")
     private String email;
+
+   @Max(value=80,message = "age cannot be > than 80")
+   @Min(value=18,message = "age of employee cannot be <18")
     private Integer age;
+
+
+   @Positive(message = "salary cannot be negative")
+   @Digits(integer = 6,fraction = 2,message = "salary can be in given form")
+   @DecimalMax(value="10000.99")
+   @DecimalMin(value="100.50")
+   private Double salary;
+
+   @NotBlank(message="Role cannot be null")
+   @Pattern(regexp="^(ADMIN|USER)",message = "role of employee can be user or admin")
+   private String role;//Admin,user
+
+    @PastOrPresent(message = "date of joining cannot be in future")
     private LocalDate dateOfJoining;
+
+    @AssertTrue(message = "Employee should be active")
     @JsonProperty("isActive")
     private Boolean isActive;
+
+
 }
 
 
